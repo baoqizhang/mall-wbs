bc4a205d9b6aa7bb4bb56b3a7f0ef57bef8f9afc
baorui.zhang <965178162@qq.com>
2021-11-15 15:39:53 +0800

    [baorui] #M-1 feat: completed product and product sku table create

---
+++ src/main/resources/db/migration/V20211115145144__create_table_product.sql
@@ -0,0 +1,12 @@
+CREATE TABLE product
+(
+   id         bigint(20) NOT NULL auto_increment comment 'id',
+   title      VARCHAR(200) NOT NULL comment '标题',
+   desc       VARCHAR(200) comment '描述',
+   saleable   boolean      NOT NULL comment '是否上架',
+   created_at datetime     NOT NULL comment '创建时间',
+   updated_at datetime     NOT NULL comment '更新时间',
+   PRIMARY KEY (id)
+);
+
+ALTER TABLE product comment '商品表';

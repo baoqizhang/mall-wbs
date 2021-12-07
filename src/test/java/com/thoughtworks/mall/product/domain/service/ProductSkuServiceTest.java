928234dd44263dc7240f07b066573a367d238b88
baorui.zhang <965178162@qq.com>
2021-12-07 09:22:40 +0800

    [baorui] #M-0 refactor: remove dependence version

--- src/test/java/com/thoughtworks/mall/product/domain/service/ProductSkuServiceTest.java
+++ src/test/java/com/thoughtworks/mall/product/domain/service/ProductSkuServiceTest.java
@@ -1,16 +1,13 @@
 package com.thoughtworks.mall.product.domain.service;
 
-import com.thoughtworks.mall.infrastructure.exception.GenericBizException;
 import com.thoughtworks.mall.infrastructure.security.common.WithMockUserImpl;
 import com.thoughtworks.mall.product.common.MockProductSku;
-import com.thoughtworks.mall.product.infrastructure.repository.ProductRepository;
 import com.thoughtworks.mall.product.infrastructure.repository.ProductSkuRepository;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 

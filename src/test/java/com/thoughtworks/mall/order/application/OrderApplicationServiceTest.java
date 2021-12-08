f34708386888da723ff09f74a8423d75a144875c
baorui.zhang <965178162@qq.com>
2021-12-08 11:32:44 +0800

    [baorui] #M-0 test: add order application test

--- src/test/java/com/thoughtworks/mall/order/application/OrderApplicationServiceTest.java
+++ src/test/java/com/thoughtworks/mall/order/application/OrderApplicationServiceTest.java
@@ -1,7 +1,7 @@
 package com.thoughtworks.mall.order.application;
 
+import com.thoughtworks.mall.infrastructure.exception.GenericBizException;
 import com.thoughtworks.mall.infrastructure.security.common.SecurityCommonProvider;
-import com.thoughtworks.mall.infrastructure.security.common.WithMockUserImpl;
 import com.thoughtworks.mall.order.common.MockOrder;
 import com.thoughtworks.mall.order.common.MockOrderDetail;
 import com.thoughtworks.mall.order.domain.entity.Order;
@@ -12,12 +12,14 @@ import com.thoughtworks.mall.order.infrastructure.enums.OrderStatus;
 import com.thoughtworks.mall.product.domain.service.ProductSkuService;
 import com.thoughtworks.mall.user.common.MockUserAddress;
 import com.thoughtworks.mall.user.domain.service.UserAddressService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -32,15 +34,27 @@ class OrderApplicationServiceTest implements MockUserAddress, MockOrder, MockOrd
    OrderApplicationService orderApplicationService = new OrderApplicationService(userAddressService, orderDetailService,
       orderService, productSkuService, securityCommonProvider);
 
+   private Order order;
+   private OrderDetail orderDetail;
+
+   @BeforeEach
+   void setUp() {
+      orderDetail = new OrderDetail(null, 1L, 111, 111, 1, Instant.now(), Instant.now());
+      order = new Order(null, 1111, 111, OrderStatus.WAIT_PAYMENT, USER_ADDRESS, List.of(orderDetail));
+   }
+
    @Test
    void should_create_success_with_right_request_info() {
-      var orderDetail = new OrderDetail(null, 1L, 111, 111, 1, Instant.now(), Instant.now());
-      var order = new Order(null, 1111, 111, OrderStatus.WAIT_PAYMENT, USER_ADDRESS, List.of(orderDetail));
-
       when(productSkuService.existsByIds(List.of(1L))).thenReturn(true);
       when(securityCommonProvider.getCurrentUserId()).thenReturn(1L);
       when(userAddressService.findByUserIdAndId(1L, 1L)).thenReturn(USER_ADDRESS);
       assertDoesNotThrow(() -> orderApplicationService.create(order, List.of(orderDetail), 1L));
+   }
+
+   @Test
+   void should_throw_generic_biz_exception_when_sku_id_not_exist() {
+      when(productSkuService.existsByIds(List.of(1L))).thenReturn(false);
+      assertThrows(GenericBizException.class, () -> orderApplicationService.create(order, List.of(orderDetail), 1L), "product sku not exist");
    }
 
 }
